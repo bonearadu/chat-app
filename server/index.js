3d39c413c12525a442f4d518f2ccdac0e7fe5af6
Radu Bonea <rab19@ic.ac.uk>
2021-03-21 17:16:02 +0200

    Add some boilerplate code for server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}.`);
-})
+});
